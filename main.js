d78d7f2240482191449bf5b6d9d068391f8168e6
Eduardo Fragoso <duds.fragoso@gmail.com>
2022-05-26 17:02:26 -0300

    criando seção sobre, serviços e adicionando icones

---
+++ main.js
@@ -0,0 +1,31 @@
+// toggle menu: hamburger and cross
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+// when clicking on a menu item, hide the menu
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+//  when scrolling change header
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+    console.log('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
