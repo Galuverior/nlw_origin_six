653c2e3cef5935c5c6338bbf4e46fc9a94b0c602
Eduardo Fragoso <duds.fragoso@gmail.com>
2022-05-27 18:07:28 -0300

    final

--- main.js
+++ main.js
@@ -18,10 +18,9 @@ for (const link of links) {
 }
 
 //  when scrolling change header
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 function changeHeaderWhenScrolling() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
-
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -36,7 +35,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 // ScrollRevealJS: Show elements when scrolling
@@ -59,9 +64,8 @@ scrollReveal.reveal(
 )
 
 // Button back to top
+const backToTopButton = document.querySelector('.back-to-top')
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
-
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
@@ -69,8 +73,34 @@ function backToTop() {
   }
 }
 
+// Active menu for visible section
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 // When scrolling
 window.addEventListener('scroll', function () {
   changeHeaderWhenScrolling()
   backToTop()
+  activateMenuAtCurrentSection()
 })
